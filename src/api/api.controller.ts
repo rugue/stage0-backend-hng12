e7bfb089f5f1b75f68629cd9934ccf48c9078d17
Osarugue Enehizena <nehirugue@gmail.com>
2025-01-28 23:43:56 +0100

    add getPublicInfo method to ApiController for public data retrieval

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('api')
-export class ApiController {}
+export class ApiController {
+  @Get()
+  getPublicInfo() {
+    return {
+      email: 'nehirugue@gmail.com',
+      current_datetime: new Date().toISOString(),
+      github_url: 'https://github.com/rugue/stage0-backend-hng12',
+    };
+  }
+}
